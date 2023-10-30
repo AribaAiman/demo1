9b81aa5fbb70e71501b5c68ae1a5a175d0414e16
Ariba <219X1A3327@gprec.ac.in>
2023-10-30 11:19:49 +0530

    First Commit

---
+++ dempush/src/pack1/Welcome.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to GIT HUB");// TODO Auto-generated method stub
+        System.out.println("We learn to push this code to git hub");
+	}
+
+}
